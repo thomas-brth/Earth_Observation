e3f4a23da4d645f2d9daa9e20fff9540f09aa38c
thomas-brth <61650988+thomas-brth@users.noreply.github.com>
2020-07-07 19:16:03 +0200

    Update water_quality.js

--- script/water_quality.js
+++ script/water_quality.js
@@ -1,33 +1,30 @@
 //VERSION=3
-function water_mask(b3, b8) {
-  var NDWI = index(b3, b8);
-  return NDWI > 0.15;
+function water_mask() {
+  var NDWI = index(B03, B08);
+  return NDWI > 0.2;
 }
 
-var wq = index(B04, B03);
-var mask = water_mask(B03, B08);
-const ramps = [
-  [-0.7, 0x0000ff],
-  [-0.1, 0x00ff00],
-  [0.4, 0xff0000]
-];
-/*
-const visualizer = new ColorRampVisualizer(ramps);
-if (mask) return visualizer.process(wq);
-else return [B04,B03,B02].map(a => a*2.5);
-*/
-let underflow_color = [0, 0, 1];
-let low_color = [0, 1, 0];
-let high_color = [1, 0 , 0];
-let overflow_color = [1, 1, 1];
-let min = -0.5;
-let mp = -0.15;
-let max = 0.0;
+// Water turbidity index
+var wt = index(B04, B03);
+// Normalized difference moisture index
+var ndmi = index(B08, B11);
+// Water mask (using NDWI)
+var mask = water_mask();
 
-return colorBlend(wq, [min, mp, max],
-[
-	underflow_color,
-	low_color,
-	high_color,
-	overflow_color // uncomment to see overflows
-]).concat(mask*dataMask);
+// Color map for water tubidity
+let underflow_color_wt = [0, 0, 1];
+let low_color_wt = [0, 1, 0];
+let high_color_wt = [1, 0 , 0];
+let overflow_color_wt = [1, 1, 1];
+let min_wt = -0.7;
+let mp_wt = -0.2;
+let max_wt = 0;
+
+// Color map for ndmi
+let ramp = [[-0.5, 0x000000],[0.5, 0xffffff]];
+var visualizer = new ColorRampVisualizer(ramp);
+
+// Display
+if (mask) return colorBlend(wt, [min_wt, mp_wt, max_wt],[underflow_color_wt, low_color_wt, high_color_wt, overflow_color_wt]);
+else return visualizer.process(ndmi);
+//else return [B04*2.9, B03*3.1, B02*3.0];
