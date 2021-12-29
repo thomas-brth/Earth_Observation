40d9155c61fd18c53cdb83538298df1c35dec51e
thomas-brth <thomasbertheau@orange.fr>
2021-12-29 12:41:09 +0100

    Adding water quality script

--- custom_scripts/example.js
+++ custom_scripts/example.js
@@ -6,46 +6,46 @@ To use as a model for all custom scripts.
 */
 
 function setup() {
-  return {
-    input: [{}], // Specify the inputs used
-    output: {}, // Sepcify the format of the output javascript object, returned by the function 'setup'
-    mosaicking: "SIMPLE" // Default value, use 'TILE' or 'ORBIT' when performing data fusion or multi-temporal analysis
-  };
+    return {
+        input: [{}], // Specify the inputs used
+        output: {}, // Sepcify the format of the output javascript object, returned by the function 'setup'
+        mosaicking: "SIMPLE" // Default value, use 'TILE' or 'ORBIT' when performing data fusion or multi-temporal analysis
+    };
 }
 
 function someFunction(arg) {
-  // To be used inside other functions
-  const ramps = [
-    [200, 0xff0000],
-    [300, 0x0000ff]
-  ];
-  const visualizer = new ColorRampVisualizer(ramps)
+    // To be used inside other functions
+    const ramps = [
+        [200, 0xff0000],
+        [300, 0x0000ff]
+    ];
+    const visualizer = new ColorRampVisualizer(ramps)
 }
 
 function preProcessScenes(collections) {
-  /*
-  Use to filter scenes, and enhance processing time. To be used only with 'TILE' or 'ORBIT' mosaicking.
-  Must return a 'collections' object.
-  */
-  var allowedDates = ["2017-05-15", "2017-06-24"];
+    /*
+    Use to filter scenes, and enhance processing time. To be used only with 'TILE' or 'ORBIT' mosaicking.
+    Must return a 'collections' object.
+    */
+    var allowedDates = ["2017-05-15", "2017-06-24"];
   
-  // Using 'TILE' mosaicking
-  collections.scenes.tiles = collections.scenes.tiles.filter(function (tile) {
-    var tileDate = tile.date.split("T")[0];
-    return allowedDates.includes(tileDate);
-  });
+    // Using 'TILE' mosaicking
+    collections.scenes.tiles = collections.scenes.tiles.filter(function (tile) {
+        var tileDate = tile.date.split("T")[0];
+        return allowedDates.includes(tileDate);
+    });
 
-  // Using 'ORBIT' mosaicking
-  collections.scenes.orbits = collections.scenes.orbits.filter(function (orbit) {
-    var orbitDateFrom = orbit.dateFrom.split("T")[0];
-    return allowedDates.includes(orbitDateFrom);
-  });
+    // Using 'ORBIT' mosaicking
+    collections.scenes.orbits = collections.scenes.orbits.filter(function (orbit) {
+        var orbitDateFrom = orbit.dateFrom.split("T")[0];
+        return allowedDates.includes(orbitDateFrom);
+    });
 
-  return collections
+    return collections
 }
 
 function evaluatePixel(samples, scenes, inputMetadata, customData, outputMetadata) {
-  // This function is the output of the script.
-  // Make sure the returned object match the described output object of the setup function.
-  return [];
+    // This function is the output of the script.
+    // Make sure the returned object match the described output object of the setup function.
+    return [];
 }
--- custom_scripts/water_quality/script.js
+++ custom_scripts/water_quality/script.js
@@ -1,30 +1,29 @@
 //VERSION=3
-function water_mask() {
-  var NDWI = index(B03, B08);
-  return NDWI > 0.2;
+
+function setup() {
+	return {
+		input: {
+			bands: ["B02", "B03", "B04", "B08"]
+		},
+		output: {
+			bands: 3
+		}
+	};
 }
 
-// Water turbidity index
-var wt = index(B04, B03);
-// Normalized difference moisture index
-var ndmi = index(B08, B11);
-// Water mask (using NDWI)
-var mask = water_mask();
+function evaluatePixel(sample) {
+	var ndwi = index(sample.B03, sample.B08);
+	var wt = index(sample.B04, sample.B03);
 
-// Color map for water tubidity
-let underflow_color_wt = [0, 0, 1];
-let low_color_wt = [0, 1, 0];
-let high_color_wt = [1, 0 , 0];
-let overflow_color_wt = [1, 1, 1];
-let min_wt = -0.7;
-let mp_wt = -0.2;
-let max_wt = 0;
+	let ramp = [
+		[-0.5, 0x000000],
+		[0.5, 0xffffff]
+	];
+	var visualizer = new ColorRampVisualizer(ramp);
 
-// Color map for ndmi
-let ramp = [[-0.5, 0x000000],[0.5, 0xffffff]];
-var visualizer = new ColorRampVisualizer(ramp);
-
-// Display
-if (mask) return colorBlend(wt, [min_wt, mp_wt, max_wt],[underflow_color_wt, low_color_wt, high_color_wt, overflow_color_wt]);
-else return visualizer.process(ndmi);
-//else return [B04*2.9, B03*3.1, B02*3.0];
+	if (ndwi > 0.2) {
+		return visualizer.process(wt);
+	} else {
+		return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02];
+	};
+}
